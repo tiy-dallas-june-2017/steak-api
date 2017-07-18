596a7f9bc624b84c65fafa6292c22eaab23f4747
Eric Sowell <eric.sowell@gmail.com>
2017-07-18 09:42:04 -0500

    better cors

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.use(express.static('public'));
 
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'steak-api.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
 
     next();
 }
